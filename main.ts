41b2bfe8c56f9295c8976dc9de808c7a33c5ac83
hawkeye1012002 <107385551+hawkeye1012002@users.noreply.github.com>
2022-06-15 14:19:40 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,62 @@
+let D5 = 0
+let D4 = 0
+let D3 = 0
+let D2 = 0
+let D1 = 0
+mbitbot_airbox.oled_init()
+mbitbot_airbox.oled_font_size(OLED_Size.size1)
+mbitbot_airbox.PMS3003_SET(soft_serial.serial_2, soft_serial.serial_1, setMode.setmode2)
+let strip = neopixel.create(DigitalPin.P12, 2, NeoPixelMode.RGB)
+basic.forever(function () {
+    mbitbot_airbox.get_pms3003(soft_serial.pms_serial_2, soft_serial.pms_serial_1)
+    basic.pause(1000)
+    D1 = mbitbot_airbox.data_pms3003(PMS5003_data.data_1)
+    D2 = mbitbot_airbox.data_pms3003(PMS5003_data.data_2)
+    D3 = mbitbot_airbox.data_pms3003(PMS5003_data.data_3)
+    D4 = mbitbot_airbox.dht11(DigitalPin.P8, DHTState.temp)
+    D5 = mbitbot_airbox.dht11(DigitalPin.P8, DHTState.humid)
+    if (D2 < 25) {
+        basic.showIcon(IconNames.Happy)
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (D2 < 35) {
+        basic.showIcon(IconNames.Asleep)
+        strip.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (D2 < 45) {
+        basic.showIcon(IconNames.Sad)
+        strip.showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else {
+        basic.showIcon(IconNames.Skull)
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+    logyun.connect_logyun(
+    soft_serial.serial_13,
+    soft_serial.serial_14,
+    "The Muscle",
+    "hawkeye101200"
+    )
+    logyun.logyun_googlesheet(
+    "1twzdFEyyjmKD6zXmi0Fp84ANBxPlXqBjzsRlyj4wNhE",
+    convertToText(D1),
+    convertToText(D2),
+    convertToText(D3),
+    convertToText(D4),
+    convertToText(D5)
+    )
+    basic.pause(1000)
+})
+control.inBackground(function () {
+    basic.pause(3000)
+    while (true) {
+        mbitbot_airbox.oled_clear()
+        mbitbot_airbox.oled_showString(0, 0, "AirBox")
+        mbitbot_airbox.oled_showString(0, 1, "PM 1.0:" + D1)
+        mbitbot_airbox.oled_showString(0, 2, "PM 2.5:" + D2)
+        mbitbot_airbox.oled_showString(0, 3, "PM 10 :" + D3)
+        basic.pause(3000)
+        mbitbot_airbox.oled_clear()
+        mbitbot_airbox.oled_showString(0, 0, "AirBox")
+        mbitbot_airbox.oled_showString(0, 1, "Temp :" + D4)
+        mbitbot_airbox.oled_showString(0, 2, "Humid :" + D5)
+        basic.pause(3000)
+    }
+})
